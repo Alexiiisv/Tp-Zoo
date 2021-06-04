fd59af6805143144f0f04204b60b01c7d337e7dd
svenickx <75172077+svenickx@users.noreply.github.com>
2021-06-04 22:24:22 +0200

    month

--- main.cpp
+++ main.cpp
@@ -8,15 +8,40 @@ using namespace std;
 
 int main()
 {
-    Zoo zoo("Zoo de la Montagne");
+    // Zoo zoo("Zoo de la Montagne");
 
-    zoo.addAnimal(new Tigre("bob"));
-    zoo.addAnimal(new Tigre("larry"));
-    zoo.addAnimal(new Poule("poule 1"));
-    zoo.addAnimal(new Poule("poule 2"));
-    zoo.addAnimal(new Aigle("aigle 1"));
-    zoo.addAnimal(new Aigle("aigle 2"));
+    // zoo.addAnimal(new Tigre("bob"));
+    // zoo.addAnimal(new Tigre("larry"));
+    // zoo.addAnimal(new Poule("poule 1"));
+    // zoo.addAnimal(new Poule("poule 2"));
+    // zoo.addAnimal(new Aigle("aigle 1"));
+    // zoo.addAnimal(new Aigle("aigle 2"));
 
-    zoo.fire();
+    // zoo.fire();
+
+    int month = 0, year = 8, nextMonth = 0;
+
+    // Tous les mois
+    while (year < 10)
+    {
+        month++;
+        if (month == 13)
+        {
+            month = 1;
+            year++;
+        }
+
+        cout << "annee : " << year << endl;
+        cout << "mois : " << month << endl;
+
+        nextMonth = 0;
+        // Action pour le mois
+        while (nextMonth != 1 && year < 10)
+        {
+            cout << "Press 1 to pass the month" << endl;
+            scanf("%d", &nextMonth);
+        }
+    }
+
     return 0;
 }
